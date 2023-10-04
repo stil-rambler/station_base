549962dc5f775b1865e08115ae2e452db112f4cc
stil-rambler <129761576+stil-rambler@users.noreply.github.com>
2023-10-04 09:00:13 +0300

    Версия 0.01

---
+++ station_base/station_base.ino
@@ -0,0 +1,33 @@
+
+
+String val;
+String stat;
+String  err1;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello!");
+err1 = "0";
+stat = "ok";
+  
+}
+void loop() {
+    if (Serial.available() > 0) {
+
+       val = Serial.readString(); // переменная val равна полученной команде
+
+       if (val == "ST") {
+          Serial.println("Version 0.1");
+          Serial.println("Status " + stat);
+          Serial.println("Error " + err1);
+       }
+       
+          Serial.println(val);
+          Serial.println("ok");
+      
+       
+
+   }
+  delay(1);
+}
